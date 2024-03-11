7401fd495258eaa061eb9dcc5c4e58ea6e24c24d
lithium <nastja.gutenthaler@gmail.com>
2024-03-11 22:24:34 +0100

    test case 3

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -42,4 +42,38 @@ class HomeControllerTest {
         expectedMovieList = expectedMovieList.stream().filter(m -> m.getGenres().contains(genre)).toList();
         assertEquals(expectedMovieList,movieList);
     }
+    @Test
+    void upper_and_lower_case_irrelevant(){
+        //given
+        HomeController homeController = new HomeController();
+        List<Movie> movieListUp = Movie.initializeMovies();
+        List<Movie> movieListLow = Movie.initializeMovies();
+        String searchTextUp = "Da";
+        String searchTextLow = "da";
+        Genres genre = null;
+
+        //when
+        movieListUp = homeController.filterMovies(movieListUp,searchTextUp,genre);
+        movieListLow = homeController.filterMovies(movieListLow,searchTextLow,genre);
+
+        //then
+        assertEquals(movieListLow,movieListUp);
+    }
+    /*@Test
+    void ascending_and_descending_filters_work(){
+        //given
+        HomeController homeController = new HomeController();
+        List<Movie> movieListAsc = Movie.initializeMovies();
+        List<Movie> movieListDesc = Movie.initializeMovies();
+        boolean desc = true;
+        boolean asc = false;
+
+        //when
+        homeController.sortMovies(movieListAsc,asc);
+        homeController.sortMovies(movieListDesc,desc);
+
+        //then
+        List<Movie> expectedAsc = Movie.initializeMovies();
+
+    }*/
 }
